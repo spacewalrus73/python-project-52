52c19fa6194e41cc1801d722794652e44d80c505
Daniil Balabanov <spacewalrus@bk.ru>
2023-09-24 22:42:07 +0300

    add users views

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,12 +1,21 @@
 from django.views import View
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from .forms import UserRegistrationForm
+from task_manager.users.models import User
+from django.views.generic.list import ListView
 # Create your views here.
 
 
-class UserIndex(View):
+class UserIndex(ListView):
+    model = User
+    paginate_by = 60
+    template_name = 'users/index.html'
+    context_object_name = 'users'
+
+
+class UserCreate(View):
 
     @staticmethod
     def get(request, *args, **kwargs):
-        all_users = User.objects.all()
-        return render(request, 'users/index.html', {'users': all_users})
+        form = UserRegistrationForm()
+        return render(request, 'users/registration.html', {'form': form})
