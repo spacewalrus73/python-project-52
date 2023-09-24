83655deb3fb4c97ae74e382f6bfb351fc51a48aa
Daniil Balabanov <spacewalrus@bk.ru>
2023-09-24 22:42:19 +0300

    add users urls

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from task_manager.users.views import UserIndex
+from task_manager.users.views import UserIndex, UserCreate
 
 urlpatterns = [
-    path('', UserIndex.as_view())
+    path('', UserIndex.as_view(), name='list_user'),
+    path('create/', UserCreate.as_view(), name='create_user'),
 ]
