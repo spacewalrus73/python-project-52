b7ffa231aefe078cae12ff71068021dfde636055
Daniil Balabanov <spacewalrus@bk.ru>
2023-09-25 21:29:23 +0300

    uses Class Based Views

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,21 +1,24 @@
-from django.views import View
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from .forms import UserRegistrationForm
 from task_manager.users.models import User
 from django.views.generic.list import ListView
-# Create your views here.
+from django.utils.translation import gettext_lazy as _
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class UserIndex(ListView):
+
     model = User
     paginate_by = 60
     template_name = 'users/index.html'
     context_object_name = 'users'
 
 
-class UserCreate(View):
+class UserCreate(CreateView, SuccessMessageMixin):
 
-    @staticmethod
-    def get(request, *args, **kwargs):
-        form = UserRegistrationForm()
-        return render(request, 'users/registration.html', {'form': form})
+    model = User
+    template_name = 'users/registration.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('create_user')
+    success_message = _("User is successfully registered")
