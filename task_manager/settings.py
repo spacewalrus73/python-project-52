4b133d4c2cfb192656090ccb278db30e9b6247ed
Daniil Balabanov <spacewalrus@bk.ru>
2023-09-25 19:29:39 +0300

    add django-extensions

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
+    'django_extensions',
     'task_manager',
     'task_manager.users',
 ]
