ba770c927d49fc0e89e159e0f7df0bc08e971af9
Daniil Balabanov <spacewalrus@bk.ru>
2023-09-25 21:26:33 +0300

    add postgresql

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -86,10 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'postgresql': dj_database_url.config(default=os.getenv("DATABASE_URL"),
+                                         conn_max_age=1800)
 }
-
-#DATABASES['default'] = dj_database_url.parse(os.getenv("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
