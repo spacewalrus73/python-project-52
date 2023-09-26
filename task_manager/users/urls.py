eb4e832fa7fb9d2739121627edbf26f321c646bd
Daniil Balabanov <spacewalrus@bk.ru>
2023-09-26 22:04:45 +0300

    add paths for update and delete users

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from task_manager.users.views import UserIndex, UserCreate
+from task_manager.users.views import UserIndex, UserCreate, UserUpdate, UserDelete
 
 urlpatterns = [
     path('', UserIndex.as_view(), name='list_user'),
     path('create/', UserCreate.as_view(), name='create_user'),
+    path('<int:pk>/update/', UserUpdate.as_view(), name='update_user'),
+    path('<int:pk>/delete/', UserDelete.as_view(), name='delete_user'),
 ]
