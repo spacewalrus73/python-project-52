4cd2737eb20da69f1ad531682ca26b74ebf14a87
Daniil Balabanov <spacewalrus@bk.ru>
2023-09-27 22:00:37 +0300

    add UserLoginView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.views import LoginView
 from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy
 
 
 class Index(TemplateView):
 
     template_name = 'home.html'
 
+
+class UserLogin(LoginView):
+
+    template_name = 'login.html'
+    next_page = reverse_lazy('index')
