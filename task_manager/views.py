ef3394a762051908434d0fda51e49303c7095033
Daniil Balabanov <spacewalrus@bk.ru>
2023-09-29 22:37:45 +0300

    Add login and logout views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,10 @@
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
 from django.views.generic.base import TemplateView
-from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class Index(TemplateView):
@@ -8,7 +12,15 @@ class Index(TemplateView):
     template_name = 'home.html'
 
 
-class UserLogin(LoginView):
+class UserLogin(SuccessMessageMixin, LoginView):
 
     template_name = 'login.html'
-    next_page = reverse_lazy('index')
+    success_message = _("You're logged in")
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
