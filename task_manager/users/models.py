9b665d8337ad0f7859d3a06cdc9fcf3c21850a85
Даниил Балабанов <spacewalrus@bk.ru>
2024-01-09 19:15:51 +0300

    fix User model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,6 +1,11 @@
+from django.db.models import DateTimeField
 from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
 class User(AbstractUser):
-    pass
+    """Default task manager's user"""
+    created_at = DateTimeField(auto_now_add=True, null=True)
+    updated_at = DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.get_full_name()
