fd31ec98ae28dbd2044f01b7f31d4665be3a2e83
Даниил Балабанов <spacewalrus@bk.ru>
2024-01-09 19:16:25 +0300

    fix User forms

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,14 +1,28 @@
 from .models import User
-from django.db.models import CharField
-from django.contrib.auth.forms import UserCreationForm
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import UserCreationForm, BaseUserCreationForm
 
 
 class UserRegistrationForm(UserCreationForm):
 
-    field_order = ['first_name', 'last_name', 'username', 'password1', 'password2']
+    field_order = [
+        'first_name',
+        'last_name',
+        'username',
+        'password1',
+        'password2',
+    ]
 
     class Meta(UserCreationForm.Meta):
-
         model = User
         fields = UserCreationForm.Meta.fields + ('first_name', 'last_name')
+
+
+class UserUpdateForm(BaseUserCreationForm):
+
+    class Meta(BaseUserCreationForm.Meta):
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+        ]
