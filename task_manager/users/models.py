d99a9fabb4a59106ea7bba7913f8c47e4e8e7282
Даниил Балабанов <spacewalrus@bk.ru>
2024-01-30 23:04:31 +0300

    delete method

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -3,6 +3,3 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     """Default task manager's user"""
-
-    def get_name(self) -> str:
-        return self.get_full_name()
