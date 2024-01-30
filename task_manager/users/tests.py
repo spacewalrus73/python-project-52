901936ef5aa5febcc05c1f22b467fec57cdb719f
Даниил Балабанов <spacewalrus@bk.ru>
2024-01-30 23:06:54 +0300

    update users tests

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,19 +1,23 @@
 from http import HTTPStatus
 
+from django.contrib.messages import Message
+from django.contrib.messages import SUCCESS
+from django.contrib.messages.test import MessagesTestMixin
 from django.test import TestCase
 from django.urls import reverse_lazy
-from django.contrib.messages.test import MessagesTestMixin
 
-from task_manager.users.models import User
 from task_manager.users.forms import UserRegistrationForm
+from task_manager.users.models import User
 
 
-class CreateUserTest(MessagesTestMixin ,TestCase):
+class CreateUserTest(MessagesTestMixin, TestCase):
     """Test create user."""
     def setUp(self):
         self.view_response = self.client.get(reverse_lazy("create_user"))
         self.form = UserRegistrationForm
-
+        self.test_name = "TestUser"
+        self.test_passwd = "000"
+        self.users_count = User.objects.count()
 
     def test_create_view_returns_correct_response(self):
         self.assertEqual(self.view_response.status_code, HTTPStatus.OK)
@@ -27,4 +31,37 @@ class CreateUserTest(MessagesTestMixin ,TestCase):
         self.assertContains(self.view_response, "password1")
         self.assertContains(self.view_response, "password2")
         self.assertContains(self.view_response, "Register")
-# TODO Check forms erros
+
+    def test_valid_registration_user(self):
+        response = self.client.post(
+            path=reverse_lazy("create_user"),
+            data={
+                "first_name": self.test_name,
+                "last_name": self.test_name,
+                "username": self.test_name,
+                "password1": self.test_passwd,
+                "password2": self.test_passwd,
+
+            },
+            follow=True
+        )
+
+        self.assertRedirects(response, reverse_lazy("login"))
+        self.assertMessages(
+            response=response,
+            expected_messages=[
+                Message(
+                    level=SUCCESS, message="User is successfully registered"
+                )
+            ]
+        )
+        # Check that user was created in db
+        self.assertNotEqual(self.users_count, User.objects.count())
+        # Check that user can log in now
+        self.client.post(
+            path=reverse_lazy("login"),
+            data={
+                "username": self.test_name,
+                "password": self.test_passwd,
+            }
+        )
