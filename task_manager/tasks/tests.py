00ef3c198e4decfc935dfa0f88da254536c99e69
Даниил Балабанов <spacewalrus@bk.ru>
2024-02-21 22:00:52 +0300

    add test

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,12 +1,14 @@
 from http import HTTPStatus
 
-from django.contrib.messages import ERROR
 from django.contrib.messages import Message
+from django.contrib.messages import SUCCESS
 from django.contrib.messages.test import MessagesTestMixin
 from django.test import TestCase
 from django.urls import reverse_lazy
 
-from task_manager.permission_mixins import UserLoginRequiredMixin as loginMixin
+from task_manager.statuses.models import Status
+from task_manager.tasks.models import Task
+from task_manager.tasks.views import TaskCreateView
 from task_manager.users.models import User
 
 
@@ -14,27 +16,11 @@ class TaskIndexTest(MessagesTestMixin, TestCase):
     """Test tasks list. Authentication required."""
 
     fixtures: list = ["test_users", "test_status", "test_tasks"]
-    list_task: str = reverse_lazy("list_task")
 
     def setUp(self):
         self.test_user: User = User.objects.first()
         self.client.force_login(self.test_user)
-        self.view_response = self.client.get(self.list_task)
-
-    def not_auth_user_cant_reach_task_list_page(self):
-        # imitate anonymous user
-        self.test_user.is_active = False
-        self.test_user.save()
-
-        view_response = self.client.get(self.list_task)
-
-        self.assertRedirects(view_response, "/login/?next=/tasks/")
-        self.assertMessages(
-            response=view_response,
-            expected_messages=[
-                Message(message=loginMixin.denied_message, level=ERROR)
-            ]
-        )
+        self.view_response = self.client.get(reverse_lazy("list_task"))
 
     def test_list_view_returns_correct_response(self):
         self.assertEqual(self.view_response.status_code, HTTPStatus.OK)
@@ -58,13 +44,66 @@ class TaskIndexTest(MessagesTestMixin, TestCase):
         self.assertContains(self.view_response, "Linus Torvalds")
         self.assertContains(self.view_response, "Guido Van Rossum")
 
-    # How to test filter????????????
-    # def test_filter_form(self):
-    #     filtered = self.client.get(
-    #         path=self.list_task,
-    #         data={
-    #             "Status": "SecondStatus",
-    #             "Performer": "Linus Torvalds"
-    #         }
+    # def test_filter_task(self):
+    #     filter_response = self.client.post(
+    #         path=reverse_lazy(viewname="list_task",
+    #                           kwargs={"status": Status.objects.first().id,
+    #                                   "performer": User.objects.first().id}),
+    #         follow=True,
     #     )
-    #     self.assertNotContains(filtered, "TestStatus")
+    #
+    #     self.assertContains(filter_response, "Task1")
+    #     self.assertNotContains(filter_response, "Task2")
+
+
+class TaskCreateTest(MessagesTestMixin, TestCase):
+    """Test task creation."""
+
+    fixtures = ["test_users", "test_status", "test_tasks"]
+    exists_error: str = "Task with this Name already exists."
+
+    def setUp(self):
+        self.test_user: User = User.objects.first()
+        self.client.force_login(self.test_user)
+        self.response = self.client.get(
+            reverse_lazy("create_task")
+        )
+
+    def test_create_view_returns_correct_response(self):
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(self.response, "form.html")
+
+    def test_create_view_contains_correct_fields(self):
+        self.assertContains(self.response, "Create task")
+        self.assertContains(self.response, "Name")
+        self.assertContains(self.response, "Description")
+        self.assertContains(self.response, "Status")
+        self.assertContains(self.response, "Performer")
+        self.assertContains(self.response, "Create")
+
+    def test_successful_creation_of_task(self):
+
+        count_before = Task.objects.count()
+
+        response = self.client.post(
+            path=reverse_lazy("create_task"),
+            data={
+                "name": "TestTask",
+                "description": "Some text.",
+                "status": Status.objects.first().id,
+                "performer": User.objects.last().id,
+            },
+            follow=True,
+        )
+        self.assertRedirects(response, reverse_lazy("list_task"))
+        self.assertMessages(
+            response=response,
+            expected_messages=[
+                Message(message=TaskCreateView.success_message, level=SUCCESS)
+            ]
+        )
+
+        count_after = Task.objects.count()
+
+        self.assertFalse(count_after == count_before)
+        self.assertEqual("TestTask", Task.objects.last().name)
