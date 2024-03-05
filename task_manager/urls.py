a29ac486d24b09c3e7d44d9f224d43575f41180e
Даниил Балабанов <spacewalrus@bk.ru>
2024-03-05 21:03:32 +0300

    include label apps routes

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
+    path('labels/', include('task_manager.labels.urls')),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('i18n/', include('django.conf.urls.i18n')),
